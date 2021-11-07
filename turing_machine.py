e4f3e65b59388ab07384d21ac0a8359238483237
@NekkittAY <37750010+NekkittAY@users.noreply.github.com>
2021-11-07 18:08:54 +0300

    Update turing_machine.py

--- turing_machine.py
+++ turing_machine.py
@@ -1,57 +1,27 @@
-class Tape(object):
-    blank = " "
-    def __init__(self, tape_string = ""):
-        self.tape = dict(enumerate(tape_string))
-        
-    def __str__(self):
-        s = ""
-        min_index = min(self.tape.keys()) 
-        max_index = max(self.tape.keys())
-        for i in range(min_index, max_index):
-            s += self.tape[i]
-        return s    
-   
-    def __getitem__(self,index):
-        if index in self.tape:
-            return self.tape[index]
-        else:
-            return Tape.blank
+class Turing_Machine:
+    def __init__(self,sTape,sPos,rules):
+        self.sTape = sTape
+        self.sPos = sPos
+        self.rules = rules
 
-    def __setitem__(self, pos, char):
-        self.tape[pos] = char
-        
-class TuringMachine(object):
-    def __init__(self, tape = "", blank = " ", init_state = "", final = None, func = None):
-        self.tape = Tape(tape)
-        self.position = 0
-        self.blank = blank 
-        self.state = init_state
-        if func == None:
-            self.func = {}
+    def tape(self):
+        self.blank = ["#"]
+        self.Tape = self.blank+self.sTape+self.blank
+
+    def Machine(self):
+        self.endTape = self.Tape
+        if not(self.Tape[self.sPos]=="#"):
+            pos = self.sPos
         else:
-            self.func = func
-        if final == None:
-            self.final = set()
-        else:
-            self.final = set(final)
-            
-    def get_tape(self):
-        return str(self.tape)
-        
-    def step(self):
-        char_position = self.tape[self.position]
-        x = (self.state, char_position)
-        if x in func:
-            y = self.func[x]
-            self.tape[self.position] = y[1]
-            if y[2] == "L":
-                self.position -= 1
-            elif y[2] == "R":
-                self.position += 1
-            self.state = y[0]
-            
-    def final_tape(self):
-        if self.state in self.final:
-            return True
-        else:
-            return False
+            pos = self.sPos+1
+        while not(self.Tape[pos]=="#"):
+            elem = self.Tape[pos]
+            self.endTape[pos] = self.rules[elem][0]
+            if self.rules[elem][1] == "L":
+                pos-=1
+            elif self.rules[elem][1] == "R":
+                pos+=1
+            else:
+                print(self.endTape)
+                break
+            print(self.endTape)
